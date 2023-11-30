c2cd72a306adbaa38b22d15e25e47920069b378f
gsspdev <dev@gssp.io>
2023-11-30 10:41:21 -0800

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+pub mod utils;
+pub mod prompt_for_params;
+pub mod oscillator;
+
+use std::io::prelude::*;
+use std::str::FromStr;
+
+pub fn parse_arg<T: FromStr>(arg: &str, description: &str) -> T {
+    arg.parse::<T>().unwrap_or_else(|_| {
+        eprintln!("Invalid {}: {}", description, arg);
+        std::process::exit(1);
+    })
+}
+
+pub fn read_param<T: FromStr>(prompt: &str) -> T {
+    print!("{}", prompt);
+    std::io::stdout().flush().unwrap();
+
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().parse::<T>() {
+            Ok(value) => return value,
+            Err(_) => {
+                print!("Invalid input. Please try again: ");
+                std::io::stdout().flush().unwrap();
+            }
+        }
+    }
+}
